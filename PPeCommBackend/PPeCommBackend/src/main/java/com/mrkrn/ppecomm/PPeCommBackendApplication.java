878cf739dab6790ed625b64429ff3a62c589255e
Irina Barciuc. <iabarchuk@edu.hse.ru>
2026-08-08 16:39:20 +0200

    Initial commit (prbbl the last one)

---
+++ PPeCommBackend/PPeCommBackend/src/main/java/com/mrkrn/ppecomm/PPeCommBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrkrn.ppecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPeCommBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PPeCommBackendApplication.class, args);
+	}
+
+}
---
+++ PPeCommBackend/PPeCommBackend/src/test/java/com/mrkrn/ppecomm/PPeCommBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrkrn.ppecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPeCommBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
